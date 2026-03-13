ee60e7ca4ddcbae5456211bf7e1209e630592d5d
Kristoffer Andersson <kristoffer.andersson@gu.se>
2026-03-13 11:54:23 +0100

    adapt run_resiliparse to look like the others

--- extractors/run_resiliparse.py
+++ extractors/run_resiliparse.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
 import gzip
-import json
 from pathlib import Path
 
+from output_util import python_dist_version, timer, write_output_json
 from resiliparse.extract.html2text import extract_plain_text
 
 
 def main():
     output = {}
-    for path in Path('html').glob('*.html.gz'):
-        with gzip.open(path, 'rt', encoding='utf8') as f:
+    for path in Path("html").glob("*.html.gz"):
+        with gzip.open(path, "rt", encoding="utf8") as f:
             html = f.read()
-        item_id = path.stem.split('.')[0]
-        output[item_id] = {'articleBody': extract_plain_text(html, main_content=True)}
-    (Path('output') / 'resiliparse.json').write_text(
-        json.dumps(output, sort_keys=True, ensure_ascii=False, indent=4),
-        encoding='utf8')
+        item_id = path.stem.split(".")[0]
+        output[item_id] = {"articleBody": extract_plain_text(html, main_content=True)}
+    write_output_json(
+        Path("output") / "resiliparse.json",
+        output=output,
+        version=python_dist_version("resiliparse"),
+    )
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    with timer("resiliparse", "Python"):
+        main()
